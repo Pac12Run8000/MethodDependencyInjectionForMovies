02660bef7a702b23b391732dbe1e3775db7a03fa
Pac12Run8000 <gangstar78666@>
2024-04-16 10:24:03 -0700

    Initial Commit

---
+++ MethodDependencyInjectionForMovies/MethodDependencyInjectionForMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MethodDependencyInjectionForMoviesApp.swift
+//  MethodDependencyInjectionForMovies
+//
+//  Created by Norbert Grover on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MethodDependencyInjectionForMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
